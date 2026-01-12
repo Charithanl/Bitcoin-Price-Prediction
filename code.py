873583f61141a2668da826ccdacee689840d517b
Charitha NL <charithanl508@gmail.com>
2026-01-12 22:45:15 +0530

    code.py updated

--- code.py
+++ code.py
@@ -54,3 +54,57 @@ df['day'] = df['Date'].dt.day
 
 # Preview result
 df.head()
+
+data_grouped = df.groupby('year').mean()
+plt.subplots(figsize=(20,10))
+for i, col in enumerate(['Open', 'High', 'Low', 'Close']):
+  plt.subplot(2,2,i+1)
+  data_grouped[col].plot.bar()
+plt.show()
+
+df['is_quarter_end'] = np.where(df['month']%3==0,1,0)
+df.head()
+
+df['open-close']  = df['Open'] - df['Close']
+df['low-high']  = df['Low'] - df['High']
+df['target'] = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
+
+plt.pie(df['target'].value_counts().values, 
+        labels=[0, 1], autopct='%1.1f%%')
+plt.show()
+
+plt.figure(figsize=(10, 10))
+
+sn.heatmap(df.corr() > 0.9, annot=True, cbar=False)
+plt.show()
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Assuming df is already defined
+features = df[['open-close', 'low-high', 'is_quarter_end']]
+target = df['target']
+
+# Scaling the features
+scaler = StandardScaler()
+features = scaler.fit_transform(features)
+
+# Split the data into training and validation (test) sets
+X_train, X_valid, Y_train, Y_valid = train_test_split(features, target, test_size=0.3, random_state=42)
+
+# 'test_size=0.3' means 30% of the data will be used for testing, and 70% for training
+
+models = [LogisticRegression(), SVC(kernel='poly', probability=True), XGBClassifier()]
+
+for i in range(3):
+  models[i].fit(X_train, Y_train)
+
+  print(f'{models[i]} : ')
+  print('Training Accuracy : ', metrics.roc_auc_score(Y_train, models[i].predict_proba(X_train)[:,1]))
+  print('Validation Accuracy : ', metrics.roc_auc_score(Y_valid, models[i].predict_proba(X_valid)[:,1]))
+  print()
+
+from sklearn.metrics import ConfusionMatrixDisplay
+
+ConfusionMatrixDisplay.from_estimator(models[0], X_valid, Y_valid, cmap='Blues')
+plt.show()
